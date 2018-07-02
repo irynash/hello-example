b2991055c693136d5f074db3fa50fe8b479f4d8f
Iryna <iryna.sashuk@gmail.com>
2018-07-02 18:24:20 +0200

    Samsung

--- src/Iphone.java
+++ src/Iphone.java
@@ -3,11 +3,13 @@ public class Iphone implements Phone1{
 
     @Override
     public void call() {
-        
+        System.out.println("MK");
+
     }
 
     @Override
     public void sms() {
+        System.out.println("KN");
 
     }
 }
---
+++ src/Samsung.java
@@ -0,0 +1,12 @@
+public class Samsung implements Phone1 {
+
+    @Override
+    public void call() {
+        System.out.println("K");
+    }
+       @Override
+        public void sms() {
+            System.out.println("N");
+
+        }
+}
