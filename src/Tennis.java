f67ec49da59cf817c51152dcb5f5ea97ef6f22c4
Iryna <iryna.sashuk@gmail.com>
2018-07-03 09:56:36 +0200

    Extending interfaces

---
+++ src/Tennis.java
@@ -0,0 +1,46 @@
+public class Tennis implements Sports, Hockey, Football {
+    @Override
+    public void setHomeTeam(String name) {
+
+    }
+
+    @Override
+    public void setVisitingTeam(String name) {
+
+    }
+
+    @Override
+    public void homeTeamScored(int points) {
+
+    }
+
+    @Override
+    public void visitingTeamScored(int points) {
+
+    }
+
+    @Override
+    public void endOfQuarter(int quarter) {
+
+    }
+
+    @Override
+    public void homeGoalScore() {
+
+    }
+
+    @Override
+    public void visitingGoalScored() {
+
+    }
+
+    @Override
+    public void endOfPeriod(int period) {
+
+    }
+
+    @Override
+    public void overtimePeriod(int ot) {
+
+    }
+}
